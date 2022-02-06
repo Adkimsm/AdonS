48fbf440127740ef7a8bd3703511b504e7686abd
Adkinsm2020 <3020035335@qq.com>
2022-02-06 12:25:25 +0800

    Edit: Turn to React.js

---
+++ src/js/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.scss";
+const app = () => {
+  return (
+    <div className="App">
+      <h1>khjjhgjhgj</h1>
+    </div>
+  );
+};
+
+export default app;
