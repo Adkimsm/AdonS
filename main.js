bde3fc8d574b1075ad39200f159a5c885cfda31d
Adkinsm2020 <3020035335@qq.com>
2022-02-06 13:22:38 +0800

    Edit: Turn to Vite + React.js

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = !app.isPackaged;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    if (isDev) {
+        win.loadURL('http://localhost:3000');
+        win.webContents.openDevTools();
+    } else {
+        win.loadFile('./dist/index.html')
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
