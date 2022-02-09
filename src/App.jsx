cf2779f3c7b450d839548825caa0c9268b4c018a
Adkinsm2020 <3020035335@qq.com>
2022-02-09 10:43:35 +0800

    Add: Add RightMenu

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import bg from './bg.jpg'
 import Header from './components/Header'
 import Dock from './components/Dock'
+import RightMenu from './components/RightMenu'
 
 let dockItems = [
 	{
@@ -40,14 +41,17 @@ let dockItems = [
 	},
 ];
 
+let menus = [{ text: "ddddd", onClick: () => alert('hhhhh')}]
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dock array={dockItems} />
-      <img src={bg} alt="background" />
-    </div>
-  )
+		<div className="App">
+			<Header />
+			<RightMenu menus={menus} />
+			<Dock array={dockItems} />
+			<img src={bg} alt="background" />
+		</div>
+	);
 }
 
 export default App
--- src/components/RightMenu.jsx
+++ src/components/RightMenu.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function RightMenu({ menus }) {
+	let [rightMenuShow, setShow] = useState("block"),
+		[rightMenuOp, setMenuOp] = useState(0),
+		[rightMenuLeft, setRightMenuLeft] = useState(0),
+		[rightMenuTop, setRightMenuTop] = useState(40);
+
+	useEffect(() => {
+		document.querySelector(".App").oncontextmenu = (event) => {
+			setShow("block");
+			setRightMenuLeft(event.pageX);
+			setRightMenuTop(event.pageY);
+			setTimeout(() => {
+				setMenuOp(1);
+			}, 100);
+		};
+		document.addEventListener("click", () => {
+			setRightMenuLeft(0);
+			setRightMenuTop(0);
+			setMenuOp(0);
+			setTimeout(() => {
+				setShow("none");
+			}, 350);
+		});
+	});
 	return (
-		<div className="rightMenu">
-			<div className="rightMenuItem">
-				<div className="rightMenuIcon"></div>
-				<span></span>
-			</div>
+		<div
+			className="rightMenu"
+			style={{
+				display: rightMenuShow,
+				opacity: rightMenuOp,
+				left: rightMenuLeft,
+				top: rightMenuTop,
+			}}
+		>
+			{menus.map((menu, index) => (
+				<div key={index} className="rightMenuItem" onClick={menu.onClick}>
+					<span>{menu.text}</span>
+				</div>
+			))}
 		</div>
 	);
 }
