b771d6afe7a113e60c27896cdbeac33656f158dc
Adkinsm2020 <3020035335@qq.com>
2022-03-06 15:46:25 +0800

    Edit: Scroll for Dock Con.

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -26,23 +26,23 @@ function Dock() {
         )
     }, 500)
     return (
-        <div className='dock'>
+        <div className='dock'
+             onWheel={event => {
+               if (
+                 (scrollLeft += event.deltaY) >
+                 window.getComputedStyle(document.body).width.replace(/px/, "").valueOf()
+               ) {
+                 alert('滚动范围超出')
+                 setScrollLeft(0)
+               } else if ((scrollLeft += event.deltaY) < 0) {
+                 alert('滚动范围超出')
+                 setScrollLeft(0)
+               } else {
+                 setScrollLeft((scrollLeft += event.deltaY))
+               }
+             }}>
             <div
                 className='dockIcons flex xCenter'
-                onWheel={event => {
-                    if (
-                        (scrollLeft += event.deltaY) >
-                        window.getComputedStyle(document.body).width.replace(/px/, "").valueOf()
-                    ) {
-                        alert('滚动范围超出')
-                        setScrollLeft(0)
-                    } else if ((scrollLeft += event.deltaY) < 0) {
-                      alert('滚动范围超出')
-                      setScrollLeft(0)
-                    } else {
-                        setScrollLeft((scrollLeft += event.deltaY))
-                    }
-                }}
                 style={{ left: scrollLeft }}
             >
                 {array.map((item, index) => (
