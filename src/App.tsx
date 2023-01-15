1367ae00b4d8724e29f3721cee8233b5d3f963b1
Adkimsm <3020035335@qq.com>
2023-01-15 16:11:07 +0800

    feat: add context menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,35 @@ import React from 'react'
 import Header from "@/Components/Header";
 import Dock from '@/Components/Dock'
 import wallpaper from 'images/wallpaper.png'
+import {
+  Menu,
+  Item,
+  useContextMenu
+} from "react-contexify";
+
+import "react-contexify/dist/ReactContexify.css";
+
+const MENU_ID = "globalMenu";
 
 const App: React.FC = () => {
-    document.title = 'AdonS'
+  document.title = 'AdonS'
+  const { show } = useContextMenu({
+    id: MENU_ID
+  });
 
-    return (
-        <>
-          <Header />
-          <img src={wallpaper} alt="" />
-          <Dock />
-        </>
-    )
+  return (<>
+    <div onContextMenu={(e) => { show({ event: e }) }}>
+      <Header />
+      <img src={wallpaper} alt="" />
+      <Dock />
+    </div>
+    <Menu id={MENU_ID} animation="scale">
+      <Item>
+        桌面设置
+      </Item>
+    </Menu>
+  </>
+  )
 }
 
 export default App
