88698fb007c7077924bd4564f1a8c3c17b75e7d2
Adkimsm <3020035335@qq.com>
2023-07-07 11:42:14 +0800

    feat: now desktop setting is draggable

--- src/Components/DesktopSettings.tsx
+++ src/Components/DesktopSettings.tsx
@@ -21,6 +21,7 @@ import {
 import { AddIcon, MinusIcon } from '@chakra-ui/icons'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import Draggable from 'react-draggable'
 import store from 'src/globalState'
 import storage from 'src/utils/functions/storage'
 import modelStyles from 'styles/components/model.module.scss'
@@ -76,78 +77,103 @@ function BasicUsage() {
                 size={size}
             >
                 <ModalOverlay />
-                <ModalContent style={{ transition: 'all .35s' }}>
-                    <ModalHeader className={modelStyles.header}>
-                        {t('DesktopSettings')}
-                    </ModalHeader>
-
+                <Draggable handle={modelStyles.header}>
                     <div
-                        className={modelStyles.maxButton}
-                        onClick={() => setSize(size == 'full' ? 'md' : 'full')}
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            zIndex: 1500,
+                        }}
                     >
-                        {size === 'md' ? <AddIcon /> : <MinusIcon />}
-                    </div>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Stack direction={'column'} spacing='24px'>
-                            <InputGroup>
-                                <InputLeftAddon
-                                    children={t('DesktopBackground')}
-                                />
-                                <Input value={value} onChange={handleChange} />
-                                <InputRightAddon
-                                    children={
-                                        <span
-                                            onClick={() => {
-                                                storage.setItem(
-                                                    'background',
-                                                    value
-                                                )
-                                                if (value == '') {
-                                                    return toast({
-                                                        title: t('succeed'),
-                                                        description:
-                                                            t(
-                                                                'defaultBackground'
-                                                            ),
-                                                        status: 'success',
-                                                        duration: 9000,
-                                                        isClosable: true,
-                                                    })
-                                                }
-                                                toast({
-                                                    title: t('succeed'),
-                                                    description:
-                                                        t('customBackground'),
-                                                    status: 'success',
-                                                    duration: 9000,
-                                                    isClosable: true,
-                                                })
-                                            }}
-                                            color='green.500'
-                                        >
-                                            {t('save')}
-                                        </span>
-                                    }
-                                />
-                            </InputGroup>
-                            <Heading as='h6' size='md'>
-                                {t('language')}
-                            </Heading>
-                            <RadioGroup
-                                onChange={setLanguageIntoStorage}
-                                value={language}
-                            >
-                                <Stack direction='row'>
-                                    <Radio value='zh'>简体中文</Radio>
-                                    <Radio value='en'>English</Radio>
-                                </Stack>
-                            </RadioGroup>
-                        </Stack>
-                    </ModalBody>
+                        <ModalContent
+                            style={{
+                                transition: 'all .35s',
+                            }}
+                        >
+                            <ModalHeader className={modelStyles.header}>
+                                {t('DesktopSettings')}
+                            </ModalHeader>
 
-                    <ModalFooter></ModalFooter>
-                </ModalContent>
+                            <div
+                                className={modelStyles.maxButton}
+                                onClick={() =>
+                                    setSize(size == 'full' ? 'md' : 'full')
+                                }
+                            >
+                                {size === 'md' ? <AddIcon /> : <MinusIcon />}
+                            </div>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <Stack direction={'column'} spacing='24px'>
+                                    <InputGroup>
+                                        <InputLeftAddon
+                                            children={t('DesktopBackground')}
+                                        />
+                                        <Input
+                                            value={value}
+                                            onChange={handleChange}
+                                        />
+                                        <InputRightAddon
+                                            children={
+                                                <span
+                                                    onClick={() => {
+                                                        storage.setItem(
+                                                            'background',
+                                                            value
+                                                        )
+                                                        if (value == '') {
+                                                            return toast({
+                                                                title: t(
+                                                                    'succeed'
+                                                                ),
+                                                                description:
+                                                                    t(
+                                                                        'defaultBackground'
+                                                                    ),
+                                                                status: 'success',
+                                                                duration: 9000,
+                                                                isClosable:
+                                                                    true,
+                                                            })
+                                                        }
+                                                        toast({
+                                                            title: t('succeed'),
+                                                            description:
+                                                                t(
+                                                                    'customBackground'
+                                                                ),
+                                                            status: 'success',
+                                                            duration: 9000,
+                                                            isClosable: true,
+                                                        })
+                                                    }}
+                                                    color='green.500'
+                                                >
+                                                    {t('save')}
+                                                </span>
+                                            }
+                                        />
+                                    </InputGroup>
+                                    <Heading as='h6' size='md'>
+                                        {t('language')}
+                                    </Heading>
+                                    <RadioGroup
+                                        onChange={setLanguageIntoStorage}
+                                        value={language}
+                                    >
+                                        <Stack direction='row'>
+                                            <Radio value='zh'>简体中文</Radio>
+                                            <Radio value='en'>English</Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                </Stack>
+                            </ModalBody>
+
+                            <ModalFooter></ModalFooter>
+                        </ModalContent>
+                    </div>
+                </Draggable>
             </Modal>
         </>
     )
